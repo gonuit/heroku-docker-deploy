5ebf80ae6415bdf0ce4d02cfdb3ab9b64a3de113
Gonuit <kamilklyta341@gmail.com>
2020-09-09 03:18:44 +0200

    Mark fields as required.

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,18 @@ import { pushDockerContainer } from './heroku/push_docker_container';
 import { releaseDockerContainer } from './heroku/release_docker_container';
 import assert from 'assert';
 
+const DEFAULT_DOCKERFILE_NAME = 'Dockerfile';
+
 (async () => {
   try {
-    const email = core.getInput('email');
-    const herokuApiKey = core.getInput('heroku_api_key');
-    const herokuAppName = core.getInput('heroku_app_name');
-    const dockerFilePath = core.getInput('dockerfile_directory');
-    const dockerfileName = core.getInput('dockerfile_name') || 'Dockerfile';
+    const email = core.getInput('email', { required: true });
+    const herokuApiKey = core.getInput('heroku_api_key', { required: true });
+    const herokuAppName = core.getInput('heroku_app_name', { required: true });
+    const dockerFilePath = core.getInput('dockerfile_directory', {
+      required: true,
+    });
+    const dockerfileName =
+      core.getInput('dockerfile_name') ?? DEFAULT_DOCKERFILE_NAME;
     const dockerOptions = core.getInput('docker_options');
 
     assert(email, 'Missing required field: `email`.');
@@ -22,7 +27,12 @@ import assert from 'assert';
     const logged = await loginToHeroku({ email, herokuApiKey });
     if (!logged) return;
 
-    const built = await buildDockerImage({ dockerfileName, dockerFilePath, dockerOptions, herokuAppName });
+    const built = await buildDockerImage({
+      dockerfileName,
+      dockerFilePath,
+      dockerOptions,
+      herokuAppName,
+    });
     if (!built) return;
 
     const pushed = await pushDockerContainer({ herokuApiKey, herokuAppName });
